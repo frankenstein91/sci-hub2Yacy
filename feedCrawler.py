4cd0409a66c402219d6862d6f8d4f110e844bfb8
Frankenstein91 <frankenstein91.info@googlemail.com>
2024-02-04 19:26:49 +0100

    first version. It only creates the URLs

--- feedCrawler.py
+++ feedCrawler.py
@@ -1,22 +1,45 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 
+# only needed for set_option
+import pandas as pd
+
 from dask import dataframe as ddf
+from dask.distributed import Client
 import argparse
 import random
+import os
 
 def main():
-    argsparser = argparse.ArgumentParser(description='Send Sci-Hub URLs to Yacy for indexing')
-    argsparser.add_argument('-i','--input', help='Input file', required=True, type=str)
-    argsparser.add_argument('--shs', help='Sci-Hub servers', required=True, nargs='+', type=str)
-
-    args = argsparser.parse_args()
+    #region argparse
+    parser = argparse.ArgumentParser(description='Send Sci-Hub URLs to Yacy for indexing')
+    parser.add_argument('-i','--input', help='Input file', required=True, type=str)
+    parser.add_argument('--shs', help='Sci-Hub servers', metavar="https://host:port", required=True, nargs='+', type=str)
+    parser.add_argument('--ys', help='YaCy servers', metavar="http://host:port", required=True, nargs='+', type=str)
+    # add arg group for Yacy user credentials
+    yacyUser = parser.add_argument_group('YaCy user credentials')
+    yacyUser.add_argument('--yuser', help='YaCy username', type=str)
+    yacyUser.add_argument('--ypass', help='YaCy password', type=str)
+    
+    args = parser.parse_args()
+    
+    if not os.path.exists(args.input):
+        print("ERROR: File not found " + args.input)
+        exit(os.EX_NOINPUT)
+    #endregion
+    
+    client = Client()
     dois = ddf.read_csv(args.input, sep='\t', header=None, names=['doi'], dtype={'doi': 'str'})
     # add URL column to the dataframe
-    dois['url'] = dois.apply(lambda row: 'http://'+random.choice(args.shs)'/'+row['doi'], axis=1, meta=('url', 'str'))
+    dois['url'] = dois.apply(lambda row: random.choice(args.shs)+'/'+row['doi'], axis=1, meta=('url', 'str'))
 
+    # create the YaCy API Call URLs
+    dois['yacy'] = dois.apply(lambda row: f"{random.choice(args.ys)}/Crawler_p.html?crawlingDomMaxPages=10000&range=wide&crawlingMode=url&crawlingURL={row['url']}&crawlingstart=NewCrawlSciHub&xsstopw=on&indexMedia=on&indexText=on&crawlingDepth=1", axis=1, meta=('yacy', 'str'))
+
+    dois = client.persist(dois)
+    pd.set_option('display.width', None)
+    pd.set_option("display.max_colwidth", None)
     print(dois.head())
-    
 
 
 if __name__ == '__main__':
